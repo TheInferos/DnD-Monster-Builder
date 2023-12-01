7fbde8518e66b4de8c3c422c72449220b8e637da
Hex C <TheInferos@users.noreply.github.com>
2023-12-01 11:27:41 +0000

    First Object Created and Main function Build

---
+++ Monster Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Monster
+{
+    public string Name { get; set; }
+    public float CR { get; set; }
+    public Monster (String name, float cr )
+    {
+        Name = name;
+        CR = cr;
+    }
+}
+class Builder
+{
+    static void Main()
+    {
+        Console.WriteLine("Test");
+        Monster Guard = new Monster("Guard", 3);
+        Console.WriteLine(Guard.Name);
+    }
+
+}
+
