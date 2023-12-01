34201b5a16dc4589dc0f6819e4e275df097061a3
Hex C <TheInferos@users.noreply.github.com>
2023-12-01 15:13:48 +0000

    Adding Basic Statlines to Monsters

--- Monster Builder/Program.cs
+++ Monster Builder/Program.cs
@@ -6,12 +6,45 @@ public class Monster
 {
     public string Name { get; set; }
     public float CR { get; set; }
-    public Monster (String name, float cr )
+    public string Size { get; set; }
+
+    public Statline Stats { get; set; }
+    public Monster(String name, float cr)
     {
         Name = name;
         CR = cr;
+        Stats = new Statline();
     }
 }
+
+    public class Statline
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Consitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+        public int Health { get; set; }
+        public int AC { get; set; }
+        public Dictionary<int, int>  HD { get; set; }
+    public Statline()
+    {
+        Strength = 10;
+        Dexterity = 10;
+        Consitution = 10;
+        Intelligence = 10;
+        Wisdom = 10;
+        Charisma = 10;
+        Health = 0;
+        AC = 10;
+        HD = new Dictionary<int, int>()
+        {
+            { 0, 0 }
+        };
+    }
+}
+
 
 
 namespace Creator
