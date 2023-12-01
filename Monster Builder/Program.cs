e2151ba629cc7330aa2a81dff5a46f2ed55b4ceb
Hex C <TheInferos@users.noreply.github.com>
2023-12-01 11:41:09 +0000

    Basic Get of Name and CR (no Error Validation)

--- Monster Builder/Program.cs
+++ Monster Builder/Program.cs
@@ -15,9 +15,13 @@ class Builder
 {
     static void Main()
     {
-        Console.WriteLine("Test");
-        Monster Guard = new Monster("Guard", 3);
-        Console.WriteLine(Guard.Name);
+
+        Console.WriteLine("What is the name of your Monster?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Thank you, and what CR should we plan for this to be?");
+        float cr = float.Parse(s: Console.ReadLine());
+        Monster monster = new Monster(name, cr);
+        Console.WriteLine($"Thank you. I have made the new monster:\n{monster.Name}\nChallenger Rating {monster.CR}");
     }
 
 }
