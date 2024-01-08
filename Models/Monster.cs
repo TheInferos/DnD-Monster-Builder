73316d963049005ac275c62fb625658dec7b3cf5
Hex C <TheInferos@users.noreply.github.com>
2024-01-08 13:06:22 +0000

    Added the XML docs for all public functions

--- Models/Monster.cs
+++ Models/Monster.cs
@@ -6,22 +6,65 @@ using Monster_Builder_Web_API.Models.DTOs;
 namespace Monster_Builder_Web_API.Models
 
 {
+    /// <summary>
+    /// This is the Class where Monsters are created there core information is held. This uses Armour, Weapon, Creature Actions, Statline
+    /// TODO: Add in Creature Class and this can extend that class for NPC/ charecters that aren't intended to 
+    /// Have the same level of detail as playable charecters
+    /// </summary>
     public class Monster
     {
+        /// <summary>
+        /// This is the name of the monster to be stored
+        /// </summary>
         public string Name { get; set; }
-        public string ID { get; set; }
-        //TODO Change to Float to account for 1/2 and 1/4
+        /// <summary>
+        /// This is the ID that the monster is stored under
+        /// </summary>
+        public string ID { get; init; }
+        /// <summary>
+        /// This contains the challenge rating of the monster. Typically this should be 0+ 
+        /// (max expected is 30 but it can go higher)
+        /// Change to Float to account for 1/2 and 1/4
+        /// </summary>
         public int CR { get; set; }
+        /// <summary>
+        /// This is the monsters Size
+        /// TODO: Switch to Enum
+        /// </summary>
         public string Size { get; set; }
+        /// <summary>
+        /// This is the monster Type 
+        /// TODO: Switch to Enum containing the options
+        /// </summary>
         public string Type { get; set; }
+        /// <summary>
+        /// This is a list of actions  that the monster has indcluding ones imported from weapons and armour
+        /// </summary>
         public List<CreatureAction> CreatureActions { get; set; }
+        /// <summary>
+        /// This is a list of weapons that the monster has.
+        /// </summary>
         public List<Weapon> Weapons { get; set; }
+        /// <summary>
+        /// This is the armour that the monster has equipt. There can only be one.
+        /// TODO: add item list for holding. That way more armours can be held even if not used
+        /// </summary>
         public Armour Armour { get; set; }
-
+        /// <summary>
+        /// This is the statline for the Monster, This is self contained and can be accessed to get the relevant Stats
+        /// </summary>
         public Statline Stats { get; set; }
+        /// <summary>
+        /// TODO, this is a work in progress, but this should be used to calculate stats
+        /// </summary>
+        private int Statpool { get; set; }
 
-        public int Statpool { get; set; }
-
+        /// <summary>
+        /// This is the constructor for monster from the Webpage
+        /// </summary>
+        /// <param name="name">This is the name of the monster</param>
+        /// <param name="cr">This is the Challenge rating of the monster. Also used to work out hitpoints</param>
+        /// <param name="hd">This is the hid die size. Used to calculate hitpoints</param>
         public Monster(string name, int cr, int hd)
         {
             ID = Guid.NewGuid().ToString();
@@ -35,12 +78,20 @@ namespace Monster_Builder_Web_API.Models
             Armour = new Armour("None");
         }
 
+        /// <summary>
+        /// This is the constructor for the Monsters to use. 
+        /// Update this when new values are added to add to storage on initial build
+        /// </summary>
         [JsonConstructor]
         public Monster()
         {
 
         }
 
+        /// <summary>
+        /// For Debugging purposes this is a pretty print of some of the Core Monster Information
+        /// </summary>
+        /// <returns>a prettified string of the monster</returns>
         public override string ToString()
         {
             string message = "";
@@ -55,18 +106,31 @@ namespace Monster_Builder_Web_API.Models
             return message.Replace("\t", "").Replace("    ", "");
         }
 
+        /// <summary>
+        /// This will add a wepaon to the monster list
+        /// </summary>
+        /// <param name="weapon">This is the weapon to add to the monster</param>
         public void AddWeapon(Weapon weapon)
         {
             Weapons.Add(weapon);
             CreatureActions.Concat(weapon.GetItemActions());
         }
 
+        /// <summary>
+        /// This is the armour that the monster will wear. This overwrites previously worn armour.
+        /// TODO: When implementing Actions on Armour hthis will need rto remove actions given. 
+        /// </summary>
+        /// <param name="armour"></param>
         public void ChangeArmour(Armour armour)
         {
             Armour = armour;
             CalculateAC();
 
         }
+        /// <summary>
+        /// This should overwrite the stats with newly provided stats and then recalculate the relevant stats
+        /// </summary>
+        /// <param name="data">This is the new statblock values for the monster</param>
         public void UpdateStats(StatblockDTO data)
         {
             Stats.ChangeStats(data);
--- Models/Statline.cs
+++ Models/Statline.cs
@@ -2,18 +2,51 @@ using Monster_Builder_Web_API.Models.DTOs;
 
 namespace Monster_Builder_Web_API.Models
 {
+    /// <summary>
+    /// This is the 5e Statline. This also includes key details such as AC, Health, Hit Die
+    /// </summary>
     public class Statline
     {
         // change to Private and setable via function
+        /// <summary>
+        /// This is the Creatures Strenth typically between 1 and 20, although can be higher than 20 cannot be lower than 0 
+        /// </summary>
         public int Strength { get;  set; }
+        /// <summary>
+        /// This is the Creatures Dexterity typically between 1 and 20, although can be higher than 20 cannot be lower than 0 
+        /// </summary>
         public int Dexterity { get; set; }
+        /// <summary>
+        /// This is the Creatures Consitution typically between 1 and 20, although can be higher than 20 cannot be lower than 0 
+        /// </summary>
         public int Consitution { get; set; }
+        /// <summary>
+        /// This is the Creatures Intelligence typically between 1 and 20, although can be higher than 20 cannot be lower than 0 
+        /// </summary>
         public int Intelligence { get; set; }
+        /// <summary>
+        /// This is the Creatures Wisdom typically between 1 and 20, although can be higher than 20 cannot be lower than 0 
+        /// </summary>
         public int Wisdom { get; set; }
+        /// <summary>
+        /// This is the Creatures Charisma typically between 1 and 20, although can be higher than 20 cannot be lower than 0 
+        /// </summary>
         public int Charisma { get; set; }
+        /// <summary>
+        /// This is the total creatures health Typically worked out from the Hit Die
+        /// </summary>
         public int Health { get; set; }
+        /// <summary>
+        /// This is the Creatures Armour Class.
+        /// </summary>
         public int AC { get; set; }
+        /// <summary>
+        /// This is a dictionary of Hit die, Where the key is the Hit Die Size and the Value in the count of the hit die
+        /// </summary>
         public Dictionary<int, int> HD { get; set; }
+        /// <summary>
+        /// This is the base constructor for the statline. If no stats are provided that it produces an empty statline.
+        /// </summary>
         public Statline()
         {
             Strength = 10;
@@ -29,6 +62,13 @@ namespace Monster_Builder_Web_API.Models
                 { 0, 0 }
             };
         }
+        /// <summary>
+        /// This is the constructor used from the webpage.
+        /// It uses the CR and Hit die to calculate an approximate health to start this is an initial total
+        /// TODO: handle updating this later
+        /// </summary>
+        /// <param name="hd">This is the Hit Die size. This is the base for size to be multiplied for health</param>
+        /// <param name="cr">This is the Challenge Rating. This is used as the count of hit die currently for a health calulcation</param>
         public Statline(int hd, int cr)
         {
             Strength = 10;
@@ -45,6 +85,12 @@ namespace Monster_Builder_Web_API.Models
             };
              CalculateHeath();
         }
+        /// <summary>
+        /// This is the full constructor for a statline taking in the DTO
+        /// </summary>
+        /// <param name="stats">This is the core 6 stats to be used</param>
+        /// <param name="ac">This is the Armour Class that should be set</param>
+        /// <param name="hd">This is the Dictionary of Hit Die and Quanitity to be used</param>
         public Statline(StatblockDTO stats, int ac, Dictionary<int, int> hd)
         {
             Strength = stats.Strength;
@@ -66,7 +112,10 @@ namespace Monster_Builder_Web_API.Models
                 Health += kvp.Value * kvp.Key;
             }
         }
-
+        /// <summary>
+        /// Given a new Statblock this function will update the statblock then recalculate the health
+        /// </summary>
+        /// <param name="data">The new statblock DTO  which the stats should mirror</param>
         public void ChangeStats(StatblockDTO data)
         {
             Strength = data.Strength;
@@ -78,6 +127,10 @@ namespace Monster_Builder_Web_API.Models
             CalculateHeath();
         }
 
+        /// <summary>
+        /// For Debugging, a pretty print of the statblock
+        /// </summary>
+        /// <returns>pretty string of stats, hp and AC for an easy read</returns>
         public override string ToString()
         {
             string message = "";
@@ -93,8 +146,14 @@ namespace Monster_Builder_Web_API.Models
             message += "\n";
             return message.Replace("\t", "").Replace("    ", "");
         }
-
-        public void GenerateStatsFromPriority(string priority, Monster monster)
+        /// <summary>
+        /// Work in Progress function. 
+        /// TBD If this is the best way to do it.
+        /// </summary>
+        /// <param name="priority"></param>
+        /// <param name="monster"></param>
+        /// <param name="statpool"></param>
+        public void GenerateStatsFromPriority(string priority, Monster monster, int statpool)
         {
             int loopCount = 1;
             int addedStats = 0;
@@ -112,7 +171,7 @@ namespace Monster_Builder_Web_API.Models
                 }
 
             }
-            while (monster.Statpool < addedStats)
+            while (statpool < addedStats)
             {
                 foreach (int Priority in PriorityList)
                 {
